037dab9e6e3a96be3cfe40b0badd652dd593800b
Eliseo Abel <eliseoabelcarh@gmail.com>
2020-04-30 18:25:15 -0300

    adding abelversion of newTienda

---
+++ VendorsMarketPlace/main.js
@@ -0,0 +1,10 @@
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+
+
+$('#table_id').DataTable( {
+    language: { search: "<i class='material-icons'>search</i>", searchPlaceholder: "Buscar o Filtrar Productos ..."},
+} );
+
+
